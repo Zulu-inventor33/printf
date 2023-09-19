52c45356dc1248256847976945561bf1392aba4f
Zulu-inventor33 <muteakelvin15@gmail.com>
2023-09-19 13:17:54 -0700

    main correction

--- main.h
+++ main.h
@@ -34,7 +34,7 @@ int rev_string(char *s);
 int _strlenc(const char *s);
 int printf_37(void);
 int printf_char(va_list val);
-int printf_string(va_list val);
+int printf_s(va_list val);
 int _putchar(char c);
 
 #endif
